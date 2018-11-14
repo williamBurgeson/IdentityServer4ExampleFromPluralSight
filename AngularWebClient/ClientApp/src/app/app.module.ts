b56ea072a4ae37011c86928bd69f732e8caacce8
Unknown <William.Burgeson@googlemail.com>
2018-11-14 23:44:49 +0100

    Integrated the front end into a dot net ng site

--- AngularWebClient/ClientApp/src/app/app.module.ts
+++ AngularWebClient/ClientApp/src/app/app.module.ts
@@ -2,33 +2,61 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {
+  MatButtonModule,
+  MatToolbarModule,
+  MatDialogModule,
+  MatTableModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatSelectModule
+} from '@angular/material';
+
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { ContactUsComponent } from './home/contact-us.component';
+import { ProjectListComponent } from './projects/project-list.component';
+import { ProjectComponent } from './projects/project.component';
+import { AddEditMilestoneDialogComponent } from './projects/add-edit-milestone-dialog.component';
+import { AdminModule } from './admin/admin.module';
+import { CoreModule } from './core/core.module';
+import { UnauthorizedComponent } from './home/unauthorized.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavMenuComponent,
     HomeComponent,
-    CounterComponent,
-    FetchDataComponent
+    ContactUsComponent,
+    ProjectListComponent,
+    ProjectComponent,
+    AddEditMilestoneDialogComponent,
+    UnauthorizedComponent
   ],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    BrowserModule,
     HttpClientModule,
     FormsModule,
-    RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
-    ])
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatDialogModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    AdminModule,
+    CoreModule,
+    AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+  ],
+  bootstrap: [AppComponent],
+  entryComponents:[
+    AddEditMilestoneDialogComponent
+  ]
 })
 export class AppModule { }
